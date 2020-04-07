884c0cc71933da0d944889726f0aa51248ed621b
son.ton <son.ton@quoine.com>
2020-04-07 20:34:30 +0700

    update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -40,26 +40,19 @@ module.exports = (on, config) => {
 
   on('file:preprocessor', webpack(options))
 
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    // `args` is an array of all the arguments that will
+    // be passed to browsers when it launches
 
-  module.exports = (on, config) => {
-    // `on` is used to hook into various events Cypress emits
-    // `config` is the resolved Cypress config
+    if (browser.family === 'chromium' && browser.name !== 'electron') {
+      // see: https://github.com/cypress-io/cypress/issues/3633
+      launchOptions.args.push('--disable-dev-shm-usage');
 
-    require('cypress-log-to-output').install(on)
+      // whatever you return here becomes the launchOptions
+      return launchOptions;
+    }
+  });
 
-    on('before:browser:launch', (browser = {}, launchOptions) => {
-      // `args` is an array of all the arguments that will
-      // be passed to browsers when it launches
-  
-      if (browser.family === 'chromium' && browser.name !== 'electron') {
-        // see: https://github.com/cypress-io/cypress/issues/3633
-        launchOptions.args.push('--disable-dev-shm-usage');
-  
-        // whatever you return here becomes the launchOptions
-        return launchOptions;
-      }
-    });
-  };
 }
 
 
